7406f81ec18901fcb023b64260a4c4f48b97f3ba
singhongithub <ajay.convict@gmail.com>
2024-01-19 16:54:38 +0530

    Add files via upload

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',83,'');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10002, 'EUR', 'INR', 75.50, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10003, 'GBP', 'INR', 95.25, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10004, 'JPY', 'INR', 0.55, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10005, 'CAD', 'INR', 62.00, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10006, 'AUD', 'INR', 55.60, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10007, 'CHF', 'INR', 81.00, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10008, 'SGD', 'INR', 57.00, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10009, 'CNY', 'INR', 12.50, '');
+
